4769ca668f8aa369a3d118950283e6b8832b8fff
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-01-03 04:43:54 +0800

    更新 QX_to_Stash.js

--- js/QX_to_Stash.js
+++ js/QX_to_Stash.js
@@ -84,7 +84,7 @@ body.forEach((x, y, z) => {
 			case "enabled=":
 				z[y - 1]?.match("#") && cron.push(z[y - 1]);
 				
-				let cronExp = x.split(" :http")[0].replace(/[^\s]+ ([^\s]+ [^\s]+ [^\s]+ [^\s]+ [^\s]+)/,'$1').replace(/(#|;|\/\/)/,'');
+				let cronExp = x.split(" http")[0].replace(/[^\s]+ ([^\s]+ [^\s]+ [^\s]+ [^\s]+ [^\s]+)/,'$1').replace(/(#|;|\/\/)/,'');
 				
 				let cronJs = x.split("://")[1].split(",")[0].replace(/(.+)/,'https://$1');
 				

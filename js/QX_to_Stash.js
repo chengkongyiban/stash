9d587e05de6a27e17062895d6be7039ac4f1130c
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-01-08 18:08:41 +0800

    更新 QX_to_Stash.js

--- js/QX_to_Stash.js
+++ js/QX_to_Stash.js
@@ -270,7 +270,7 @@ ${providers}`
 		.replace(/(#.+\n)\n/g,'$1')
 		.replace(/\n{2,}/g,'\n\n')
 
-$notification.post("不支持的类型已跳过","第" + others,"",req)
+$notification.post("不支持的类型已跳过","第" + others,"点击查看原文，长按查看跳过行",req)
 
  $done({ response: { status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } });
 

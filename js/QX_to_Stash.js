3754dd1a22260758450dcb82a49a964d41de95e4
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-01-09 19:33:11 +0800

    更新 QX_to_Stash.js

--- js/QX_to_Stash.js
+++ js/QX_to_Stash.js
@@ -8,7 +8,7 @@
    t&hn; = 四个空格 - 一个空格
    t&zd; = {  , }  花括号中的逗号
 ***************************/
-
+const ua = $request.headers['User-Agent'] || $request.headers['user-agent']
 const isStashiOS = 'undefined' !== typeof $environment && $environment['stash-version'] && ua.indexOf('Macintosh') === -1
 const isSurgeiOS = 'undefined' !== typeof $environment && $environment['surge-version'];
 const isShadowrocket = 'undefined' !== typeof $rocket;

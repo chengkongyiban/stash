4f933e622a88b5c5573b2c6477564a42766ce3f1
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-01-10 23:54:26 +0800

    更新 Surge_to_Stash.js

--- js/Surge_to_Stash.js
+++ js/Surge_to_Stash.js
@@ -38,6 +38,14 @@ desc = "desc: " + decodeURIComponent(desc);
 
 !(async () => {
   let body = await http(req);
+//判断是否断网
+if(body == null){if(isSurgeiOS || isStashiOS){
+	$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
+ $done({ response: { status: 404 ,body:{} } });}else{$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
+ $done({ response: { status: 404 ,body:{} } });
+}//识别客户端通知
+}else{//以下开始重写及脚本转换
+	
 original = body.split("\n");
 	body = body.match(/[^\n]+/g);
 
@@ -349,6 +357,7 @@ if (isSurgeiOS || isStashiOS) {
        others !="" && $notification.post("不支持的类型已跳过","第" + others,"点击查看原文，长按可展开查看跳过行",req);}};
 
  $done({ response: { status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } });
+}//判断是否断网的反括号
 
 })()
 .catch((e) => {

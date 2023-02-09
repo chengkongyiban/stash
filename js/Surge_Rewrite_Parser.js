e99d85fa11ed0a6c9901d604486e5e2473d694e9
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-02-09 16:58:04 +0800

    更新 Surge_Rewrite_Parser.js

--- js/Surge_Rewrite_Parser.js
+++ js/Surge_Rewrite_Parser.js
@@ -57,7 +57,7 @@ let randomStickerNum = parseInt(stickerStartNum + Math.random() * stickerSum).to
 !(async () => {
   let body = await http(req);
 //判断是否断网
-if(body == null){if(isSurgeiOS || isStashiOS){
+if(body == null){if(isStashiOS){
 	$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
  $done({ response: { status: 404 ,body:{} } });}else{$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
  $done({ response: { status: 404 ,body:{} } });
@@ -523,7 +523,7 @@ ${script}
 
 ${MITM}`
 		.replace(/t&zd;/g,',')
-		.replace(/(#.+\n)\n+/g,'$1')
+		.replace(/(#.+\n)\n+(?!\[)/g,'$1')
 		.replace(/\n{2,}/g,'\n\n')
 }else if (isStashiOS){
     rules = (rules[0] || '') && `rules:\n${rules.join("\n")}`;
@@ -574,7 +574,7 @@ ${providers}`
 		.replace(/script-providers:\n+$/g,'')
 		.replace(/#      \n/gi,'\n')
 		.replace(/      \n/g,"")
-		.replace(/(#.+\n)\n+/g,'$1')
+		.replace(/(#.+\n)\n+(?!\[)/g,'$1')
 		.replace(/\n{2,}/g,'\n\n')
 }
 

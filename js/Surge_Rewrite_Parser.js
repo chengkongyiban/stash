3f1549f3c5711ab00db1e3c92a8ef00246c20638
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-06-05 09:38:58 +0800

    更新 Surge_Rewrite_Parser.js

--- js/Surge_Rewrite_Parser.js
+++ js/Surge_Rewrite_Parser.js
@@ -72,10 +72,10 @@ let randomStickerNum = parseInt(stickerStartNum + Math.random() * stickerSum).to
 //判断是否断网
 if(body == null){if(isStashiOS){
     console.log("Surge转换：未获取到body的链接为" + $request.url)
-	$notification.post("Surge转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
+	$notification.post("Surge转换：未获取到body","请检查网络及节点是否畅通\n" + "源链接为" + $request.url,"认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
  $done({ response: { status: 404 ,body:{} } });}else{
     console.log("Surge转换：未获取到body的链接为" + $request.url)
-    $notification.post("Surge转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
+    $notification.post("Surge转换：未获取到body","请检查网络及节点是否畅通\n" + "源链接为" + $request.url,"认为是bug?点击通知反馈","https://t.me/zhangpeifu")
  $done({ response: { status: 404 ,body:{} } });
 }//识别客户端通知
 }else{//以下开始重写及脚本转换

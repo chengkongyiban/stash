0bb4bb2d895bc05a456eb9c78acfed7b5c1e42da
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-06-05 09:33:21 +0800

    更新 QX_Rewrite_Parser.js

--- js/QX_Rewrite_Parser.js
+++ js/QX_Rewrite_Parser.js
@@ -81,10 +81,10 @@ let randomStickerNum = parseInt(stickerStartNum + Math.random() * stickerSum).to
 //判断是否断网
 if(body == null){if(isSurgeiOS || isLanceX || isStashiOS || isEgern){
     console.log("QX转换：未获取到body的链接为" + $request.url)
-	$notification.post("QX转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
+	$notification.post("QX转换：未获取到body","请检查网络及节点是否畅通\n" + "源链接为" + $request.url,"认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
  $done({ response: { status: 404 ,body:{} } });}else{
     console.log("QX转换：未获取到body的链接为" + $request.url)
-    $notification.post("QX转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
+    $notification.post("QX转换：未获取到body","请检查网络及节点是否畅通\n" + "源链接为" + $request.url,"认为是bug?点击通知反馈","https://t.me/zhangpeifu")
  $done({ response: { status: 404 ,body:{} } });
 }//识别客户端通知
 }else{//以下开始重写及脚本转换

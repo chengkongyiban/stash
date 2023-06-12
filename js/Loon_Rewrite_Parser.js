ea998b91e677a1b9989d0b2349d2729d469e2ee3
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-06-13 04:28:48 +0800

    更新 Loon_Rewrite_Parser.js

--- js/Loon_Rewrite_Parser.js
+++ js/Loon_Rewrite_Parser.js
@@ -143,7 +143,7 @@ if (isLooniOS || isSurgeiOS || isShadowrocket){
 		switch (type) {
 			case "#!":
             if (isStashiOS){
-                x = x.replace(/#![^nd].+/,"").replace(/#!/,"").replace(/:/i,"：").replace(/ *= */,": ");
+                x = x.replace(/#![^nd].+/,"").replace(/#!/,"").replace(/(name|desc) *= *([^\s]+)/,'$1: "$2"');
                 pluginDesc.push(x);
             }else{
             pluginDesc.push(x);

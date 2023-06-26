84a8c7def3b773dc6dc7dff3a513ef81177af30a
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-06-27 02:09:14 +0800

    更新 Surge_Rewrite_Parser.beta.js

--- js/Surge_Rewrite_Parser.beta.js
+++ js/Surge_Rewrite_Parser.beta.js
@@ -296,7 +296,7 @@ if (isLooniOS || isSurgeiOS || isShadowrocket){
 				z[y - 1]?.match(/^#/) && tiles.push("    " + z[y - 1]);
 				
 				tiles.push(
-					`${noteK2}- name: "${scname}_${y}"${noteKn4}interval: 3600${noteKn4}title: "${scname}"${noteKn4}icon: "${tilesIcon}"${noteKn4}backgroundColor: "${tilesColor}"${noteKn4}timeout: 30${noteKn4}${arg}`);
+					`${noteK2}- name: "${scname}_${y}"${noteKn4}interval: 3600${noteKn4}title: "${scname}"${noteKn4}icon: "${tilesIcon}"${noteKn4}backgroundColor: "${tilesColor}"${noteKn4}timeout: 30${arg}`);
 			providers.push(
 					`${noteK2}${scname}_${y}:${noteKn4}url: ${js}${noteKn4}interval: 86400`);
             };

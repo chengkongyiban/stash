6ef4e32382874971101d0ac70f2f679948ac6ed6
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-07-01 17:23:04 +0800

    更新 Loon_Rewrite_Parser.js

--- js/Loon_Rewrite_Parser.js
+++ js/Loon_Rewrite_Parser.js
@@ -16,6 +16,8 @@ const iconLibrary1 = $persistentStore.read("插件随机图标合集") ?? "Dorae
 const iconLibrary2 = iconLibrary1.split("(")[0];
 
 var pluginPokemonIcon
+var pluginPokemonAuthor
+var pluginPokemonHomepage
 //宝可梦插件图标game
 if (isLooniOS && iconLibrary2 == "Pokemon" && iconStatus == "启用"){
     //初阶宝可梦
@@ -25,7 +27,6 @@ var beginnerPokemon = [1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 101
 const pokemonList = [{"name": "波克比","number": "0175","icon": 1001},{"name": "穿山鼠","number": "0027","icon": 1002},{"name": "穿山鼠-阿罗拉的样子","number": "0027","icon": 1003},{"name": "六尾","number": "0037","icon": 1004},{"name": "六尾-阿罗拉的样子","number": "0037","icon": 1005},{"name": "胖丁","number": "0039","icon": 1006},{"name": "超音蝠","number": "0041","icon": 1007},{"name": "毛球","number": "0048","icon": 1008},{"name": "喵喵","number": "0052","icon": 1009},{"name": "可达鸭","number": "0054","icon": 1010},{"name": "小火马","number": "0077","icon": 1011},{"name": "小火马-伽勒尔的样子","number": "0077","icon": 1012},{"name": "呆呆兽","number": "0079","icon": 1013},{"name": "鲤鱼王","number": "0129","icon": 1014},{"name": "臭泥","number": "0088","icon": 1015},{"name": "大岩蛇","number": "0095","icon": 1016},{"name": "卡拉卡拉","number": "0104","icon": 1017},{"name": "瓦斯弹","number": "0109","icon": 1018},{"name": "角金鱼","number": "0118","icon": 1019},{"name": "海星星","number": "0120","icon": 1020},{"name": "飞天螳螂","number": "0123","icon": 1021},{"name": "伊布","number": "0133","icon": 1100},{"name": "妙蛙种子","number": "0001","icon": 1101},{"name": "杰尼龟","number": "0007","icon": 1102},{"name": "皮丘","number": "0172","icon": 1103},{"name": "菊草叶","number": "0152","icon": 1104},{"name": "独角虫","number": "0013","icon": 1105},{"name": "波波","number": "0016","icon": 1106},{"name": "凯西","number": "0063","icon": 1107},{"name": "喇叭芽","number": "0069","icon": 1108},{"name": "鬼斯","number": "0092","icon": 1109},{"name": "拉鲁拉丝","number": "0280","icon": 1110},{"name": "绿毛虫","number": "0010","icon": 1199},{"name": "小火龙","number": "0004","icon": 1200},{"name": "拉普拉斯","number": "0131","icon": 1201},{"name": "果然翁","number": "0202","icon": 1202},{"name": "正电拍拍","number": "0311","icon": 1203},{"name": "负电拍拍","number": "0312","icon": 1204},{"name": "大葱鸭","number": "0083","icon": 1205},{"name": "波克基古","number": "0176","icon": 1301},{"name": "穿山王","number": "0028","icon": 1302},{"name": "穿山王-阿罗拉的样子","number": "0028","icon": 1303},{"name": "九尾","number": "0038","icon": 1304},{"name": "九尾-阿罗拉的样子","number": "0038","icon": 1305},{"name": "胖可丁","number": "0040","icon": 1306},{"name": "大嘴蝠","number": "0042","icon": 1307},{"name": "摩鲁蛾","number": "0049","icon": 1308},{"name": "猫老大","number": "0053","icon": 1309},{"name": "哥达鸭","number": "0055","icon": 1310},{"name": "烈焰马","number": "0078","icon": 1311},{"name": "烈焰马-伽勒尔的样子","number": "0078","icon": 1312},{"name": "呆壳兽","number": "0080","icon": 1313},{"name": "暴鲤龙","number": "0130","icon": 1314},{"name": "臭臭泥","number": "0089","icon": 1315},{"name": "大钢蛇","number": "0208","icon": 1316},{"name": "嘎啦嘎啦","number": "0105","icon": 1317},{"name": "双弹瓦斯","number": "0110","icon": 1318},{"name": "金鱼王","number": "0119","icon": 1319},{"name": "宝石海星","number": "0121","icon": 1320},{"name": "巨钳螳螂","number": "0212","icon": 1321},{"name": "水伊布","number": "0134","icon": 1393},{"name": "雷伊布","number": "0135","icon": 1394},{"name": "火伊布","number": "0136","icon": 1395},{"name": "太阳伊布","number": "0196","icon": 1396},{"name": "月亮伊布","number": "0197","icon": 1397},{"name": "叶伊布","number": "0470","icon": 1398},{"name": "冰伊布","number": "0471","icon": 1399},{"name": "仙子伊布","number": "0700","icon": 1400},{"name": "妙蛙草","number": "0002","icon": 1401},{"name": "卡咪龟","number": "0008","icon": 1402},{"name": "皮卡丘","number": "0025","icon": 1403},{"name": "月桂叶","number": "0153","icon": 1404},{"name": "铁壳蛹","number": "0014","icon": 1405},{"name": "比比鸟","number": "0017","icon": 1406},{"name": "勇基拉","number": "0064","icon": 1407},{"name": "口呆花","number": "0070","icon": 1408},{"name": "鬼斯通","number": "0093","icon": 1409},{"name": "奇鲁莉安","number": "0281","icon": 1410},{"name": "铁甲蛹","number": "0011","icon": 1499},{"name": "火恐龙","number": "0005","icon": 1500},{"name": "妙蛙花","number": "0003","icon": 1501},{"name": "水箭龟","number": "0009","icon": 1502},{"name": "雷丘","number": "0026","icon": 1503},{"name": "大竺葵","number": "0154","icon": 1504},{"name": "超级大针蜂","number": "0015","icon": 1505},{"name": "大比鸟","number": "0018","icon": 1506},{"name": "胡地","number": "0065","icon": 1507},{"name": "大食花","number": "0071","icon": 1508},{"name": "耿鬼","number": "0094","icon": 1509},{"name": "沙奈朵","number": "0282","icon": 1510},{"name": "巴大蝶","number": "0012","icon": 1596},{"name": "巴大蝶-超级巨化","number": "0012","icon": 1597},{"name": "喷火龙","number": "0006","icon": 1598},{"name": "超级喷火龙X","number": "0006","icon": 1599},{"name": "超级喷火龙Y","number": "0006","icon": 1600},{"name": "梦幻","number": "0151","icon": 1701},{"name": "超梦","number": "0150","icon": 1702},{"name": "急冻鸟","number": "0144","icon": 1703},{"name": "闪电鸟","number": "0145","icon": 1704},{"name": "火焰鸟","number": "0146","icon": 1705},{"name": "盔甲鸟","number": "0227","icon": 1706},{"name": "雷公","number": "0243","icon": 1707},{"name": "炎帝","number": "0244","icon": 1708},{"name": "水君","number": "0245","icon": 1709},{"name": "洛奇亚","number": "0249","icon": 1710},{"name": "凤王","number": "0250","icon": 1711},{"name": "盖欧卡","number": "0382","icon": 1712},{"name": "固拉多","number": "0383","icon": 1713},{"name": "帕路奇犽","number": "0484","icon": 1714},{"name": "帝牙卢卡","number": "0483","icon": 1715},{"name": "烈空坐","number": "0384","icon": 1716}];
 
 var pokemonPBUrl = "https://www.pokemon.cn/play/pokedex/";
-	
 
 //宝可梦卡池
 var pokemonCdp = [];
@@ -46,16 +47,23 @@ if ($persistentStore.read("Pokemon_count") == null || $persistentStore.read("Pok
     var result = getArrayItems(pokemonCdp, 1);
     var num = result[0];
     count[num] = (count[num] || 0) + 1;
-$persistentStore.write(JSON.stringify(count), "Pokemon_count")
-    pluginPokemonIcon = "https://raw.githubusercontent.com/Toperlock/Quantumult/main/icon/Pokemon/Pokemon-" + result + ".png"
+$persistentStore.write(JSON.stringify(count), "Pokemon_count");
+	var pokemonInfo = getPokemonByIcon(result[0]);
+    pluginPokemonIcon = "https://raw.githubusercontent.com/Toperlock/Quantumult/main/icon/Pokemon/Pokemon-" + result + ".png";
+	pluginPokemonAuthor = "#!author=" + pokemonInfo.name;
+	pluginPokemonHomepage = "#!homepage=" + pokemonPBUrl + pokemonInfo.number;
 }else{
 	$persistentStore.read("Pokemon_count")
 	count = JSON.parse($persistentStore.read("Pokemon_count"))
 	var result = getArrayItems(pokemonCdp, 1);
     var num = result[0];
     count[num] = (count[num] || 0) + 1;
+	console.log(result)
 	$persistentStore.write(JSON.stringify(count), "Pokemon_count")
-    pluginPokemonIcon = "https://raw.githubusercontent.com/Toperlock/Quantumult/main/icon/Pokemon/Pokemon-" + result + ".png"
+	var pokemonInfo = getPokemonByIcon(result[0]);
+    pluginPokemonIcon = "https://raw.githubusercontent.com/Toperlock/Quantumult/main/icon/Pokemon/Pokemon-" + result + ".png";
+	pluginPokemonAuthor = "#!author=" + pokemonInfo.name;
+	pluginPokemonHomepage = "#!homepage=" + pokemonPBUrl + pokemonInfo.number;
 };
 
 
@@ -399,7 +407,17 @@ if (isLooniOS || isSurgeiOS || isShadowrocket){
             pluginDesc.push(x);
             };
             
-            if (isLooniOS || isSurgeiOS || isShadowrocket){
+			if (isLooniOS && iconStatus == "启用" && iconLibrary2 == "Pokemon"){
+				if (nName != null){
+                x = x.replace(/^#!name *=.*/,name).replace(/^#!desc *=.*/,desc);};
+            if (iconReplace == "启用"){
+                x = x.replace(/^#!icon *=.*/,pluginIcon);
+            };
+			x = x.replace(/^(#!author *=).*/i,pluginPokemonAuthor)
+			x = x.replace(/^(#!homepage *=).*/i,pluginPokemonHomepage)
+            pluginDesc.push(x);
+				
+			}else if (isLooniOS || isSurgeiOS || isShadowrocket){
             if (nName != null){
                 x = x.replace(/^#!name *=.*/,name).replace(/^#!desc *=.*/,desc);};
             if (iconReplace == "启用"){
@@ -757,14 +775,28 @@ if (isLooniOS){
     pluginDesc = (pluginDesc[0] || '') && `${pluginDesc.join("\n")}`;
     
     if (pluginDesc !="" && pluginDesc.search(/#! *name *=/) != -1){
-        
+        //没有图标的插入图标
         if (pluginDesc.search(/#! *icon *= *.+/) == -1){
         pluginDesc = pluginDesc + "\n" + pluginIcon;
             
         }else{pluginDesc = pluginDesc;};
-        
+		
+        //Pokemon没有作者的插入作者
+        if (iconLibrary2 == "Pokemon" && pluginDesc.search(/#! *author *= *.+/i) == -1){
+        pluginDesc = pluginDesc + "\n" + pluginPokemonAuthor;
+        }else{pluginDesc = pluginDesc;};
+		
+        //Pokemon没有homepage的插入homepage
+        if (iconLibrary2 == "Pokemon" && pluginDesc.search(/#! *homepage *= *.+/i) == -1){
+        pluginDesc = pluginDesc + "\n" + pluginPokemonHomepage;
+        }else{pluginDesc = pluginDesc;};
+		
     }else{
-        pluginDesc = npluginDesc + "\n" + pluginIcon;
+        if (iconLibrary2 == "Pokemon"){
+            pluginDesc = npluginDesc + "\n" + pluginIcon + "\n" + pluginPokemonAuthor + "\n" + pluginPokemonHomepage;
+        }else{
+                    pluginDesc = npluginDesc + "\n" + pluginIcon;
+        };
     };
     
     if (iconReplace == "启用" && pluginDesc.search(/#!icon=/) == -1 ){

a9d6fbf87bd43cd7337a9146a04fc5b8cdcf31e1
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-07-09 00:47:23 +0800

    更新 Loon_Rewrite_Parser.js

--- js/Loon_Rewrite_Parser.js
+++ js/Loon_Rewrite_Parser.js
@@ -364,7 +364,7 @@ $persistentStore.write(JSON.stringify(oCache), 'parser_cache');
 if(body == null || body == ""){if(isStashiOS || isSurgeiOS){
     console.log("Loon转换：未获取到body的链接为" + $request.url)
 	$notification.post("Loon转换：未获取到body","请检查网络及节点是否畅通\n" + "源链接为" + $request.url,"认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
- $done({ response: { status: 404 ,body:{} } });}else{
+ $done({ response: { status: 404 ,body:{} } });}else if(isLooniOS || isShadowrocket){
     console.log("Loon转换：未获取到body的链接为" + $request.url)
     $notification.post("Loon转换：未获取到body","请检查网络及节点是否畅通\n" + "源链接为" + $request.url,"认为是bug?点击通知反馈","https://t.me/zhangpeifu")
  $done({ response: { status: 404 ,body:{} } });

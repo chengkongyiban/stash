892058edeae55844b227dc105cb44ec63db9b7ab
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-07-10 17:06:43 +0800

    更新 loon-rewrite.beta3.js

--- stoverride/loon-rewrite.beta3.js
+++ stoverride/loon-rewrite.beta3.js
@@ -952,14 +952,13 @@ for (let i = 0; i < scriptBox.length; i++) {
 		noteKn4 = "\n#    ";noteKn6 = "\n#      ";noteK2 = "#  ";
 	}else{noteKn4 = "\n    ";noteKn6 = "\n      ";noteK2 = "  ";}
 	if (scriptBox[i].matchptn !== undefined){
-		console.log(scriptBox[i].noteK)
-	script.push(`${noteKn4}- match: ` + scriptBox[i].matchptn + `${noteKn6}name: ` + scriptBox[i].name + `${noteKn6}type: ` + scriptBox[i].type + `${noteKn6}require-body: ` + scriptBox[i].requirebody + `${noteKn6}max-size: ` + scriptBox[i].maxsize + `${noteKn6}binary-mode: ` + scriptBox[i].binarymode + `${noteKn6}` + scriptBox[i].argument);
+	script.push(`${noteKn4}- match: ` + scriptBox[i].matchptn + `${noteKn6}name: "` + scriptBox[i].name + `"${noteKn6}type: ` + scriptBox[i].type + `${noteKn6}timeout: 30` + `${noteKn6}require-body: ` + scriptBox[i].requirebody + `${noteKn6}max-size: ` + scriptBox[i].maxsize + `${noteKn6}binary-mode: ` + scriptBox[i].binarymode + `${noteKn6}` + scriptBox[i].argument);
 	
-	providers.push(`${noteK2}` + scriptBox[i].name + ":" + `${noteKn4}url: ` + scriptBox[i].jsurl)
+	providers.push(`${noteK2}"` + scriptBox[i].name + '":' + `${noteKn4}url: ` + scriptBox[i].jsurl + `${noteKn4}interval: 86400`);
 	}else{
-		cron.push(`${noteKn4}- name: ` + scriptBox[i].name + `${noteKn6}cron: "` + scriptBox[i].cronExp + `"${noteKn6}timeout: 60`);
+		cron.push(`${noteKn4}- name: "` + scriptBox[i].name + `"${noteKn6}cron: "` + scriptBox[i].cronExp + `"${noteKn6}timeout: 60`);
 		
-		providers.push(`${noteK2}` + scriptBox[i].name + ":" + `${noteKn4}url: ` + scriptBox[i].jsurl);
+		providers.push(`${noteK2}"` + scriptBox[i].name + '":' + `${noteKn4}url: ` + scriptBox[i].jsurl + `${noteKn4}interval: 86400`);
 	}
 
 };
